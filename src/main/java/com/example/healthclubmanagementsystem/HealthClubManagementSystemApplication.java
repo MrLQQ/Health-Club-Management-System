a8d2abfcb56359facf4f33dc43f38c7ed8472efc
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-22 21:08:29 +0800

    first commit

---
+++ src/main/java/com/example/healthclubmanagementsystem/HealthClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthclubmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthClubManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthClubManagementSystemApplication.class, args);
+    }
+
+}

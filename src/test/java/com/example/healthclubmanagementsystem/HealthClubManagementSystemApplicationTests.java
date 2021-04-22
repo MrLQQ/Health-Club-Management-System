b5a0b6fd17ea45357112734a0805024174a676fc
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-22 21:08:29 +0800

    first commit

---
+++ src/test/java/com/example/healthclubmanagementsystem/HealthClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthclubmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthClubManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

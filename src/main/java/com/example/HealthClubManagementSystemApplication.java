2f58abb15236aa975968d2ba84d21e884c514df6
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-23 23:31:05 +0800

    添加对mapper的路径扫描

--- src/main/java/com/example/HealthClubManagementSystemApplication.java
+++ src/main/java/com/example/HealthClubManagementSystemApplication.java
@@ -1,8 +1,10 @@
-package com.example.healthclubmanagementsystem;
+package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.example.mapper")
 @SpringBootApplication
 public class HealthClubManagementSystemApplication {
 

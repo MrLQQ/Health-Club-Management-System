ad28cc242dd5b4100b7d6ea1e1319a756a71c3d4
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-25 09:31:26 +0800

    选课实体

---
+++ src/main/java/com/example/pojo/Pick.java
@@ -0,0 +1,16 @@
+package com.example.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("Pick")
+public class Pick {
+
+    private Long userID;
+    private Long courseID;
+}

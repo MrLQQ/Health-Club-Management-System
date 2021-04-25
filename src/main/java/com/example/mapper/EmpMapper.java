ca6f0e72ee03108d15f446e41184cf2e965ff17c
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-25 09:30:22 +0800

    员工Mapper

---
+++ src/main/java/com/example/mapper/EmpMapper.java
@@ -0,0 +1,9 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.pojo.Emp;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpMapper extends BaseMapper<Emp> {
+}

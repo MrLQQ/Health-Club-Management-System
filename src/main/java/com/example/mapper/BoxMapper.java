cfa103dca393ca0e7d53589fb9e6281dd982d1ee
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-25 09:26:48 +0800

    储物柜Mapper

---
+++ src/main/java/com/example/mapper/BoxMapper.java
@@ -0,0 +1,9 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.pojo.Box;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoxMapper extends BaseMapper<Box> {
+}

4280f5221106a579e5b8473007dac469457f3247
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-28 17:09:08 +0800

    添加user的createTime字段；添加创建时间自动填充

--- src/main/java/com/example/pojo/Pick.java
+++ src/main/java/com/example/pojo/Pick.java
@@ -1,9 +1,13 @@
 package com.example.pojo;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +17,8 @@ public class Pick {
 
     private Long userID;
     private Long courseID;
+
+    /*自动填充，选课的时间*/
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
 }

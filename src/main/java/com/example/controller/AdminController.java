af103fc941054e1167d84e9179b49913b09de4dc
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-04-29 11:30:03 +0800

    增加login API

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -1,7 +1,14 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
+
+    @RequestMapping("/login")
+    public String Login(){
+        return "";
+    }
 }

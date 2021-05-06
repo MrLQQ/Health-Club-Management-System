2726a8e30f619723b619791dee7a957240a527f9
liyuxi <2328181539@qq.com>
2021-05-06 11:04:26 +0800

    LYX

---
+++ Health-Club-Management-System-master/src/main/java/com/example/HealthClubManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthClubManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthClubManagementSystemApplication.class, args);
+    }
+
+}
---
+++ Health-Club-Management-System-master/src/main/java/com/example/mapper/CourseMapper.java
@@ -0,0 +1,9 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.pojo.Course;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMapper extends BaseMapper<Course> {
+}

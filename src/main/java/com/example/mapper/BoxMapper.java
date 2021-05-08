1c91094d6b5055857daa8606f0ad40ea4d536c05
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 00:10:07 +0800

    储物柜mapper

--- src/main/java/com/example/mapper/BoxMapper.java
+++ src/main/java/com/example/mapper/BoxMapper.java
@@ -1,18 +1,9 @@
 package com.example.mapper;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.pojo.Box;
-import com.example.pojo.BoxUserVO;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BoxMapper extends BaseMapper<Box> {
-
-    @Select("SELECT Box.*,(select userName from User where Box.userID=User.userID) as userName FROM Box")
-    List<BoxUserVO> getBoxInfo(Page<BoxUserVO> page);
 }

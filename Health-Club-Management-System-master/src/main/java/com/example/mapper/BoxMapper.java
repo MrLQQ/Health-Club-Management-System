40044a63897a732538a7029d874120d5134ecf0b
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 00:10:07 +0800

    储物柜mapper

--- Health-Club-Management-System-master/src/main/java/com/example/mapper/BoxMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.pojo.Box;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoxMapper extends BaseMapper<Box> {
-}

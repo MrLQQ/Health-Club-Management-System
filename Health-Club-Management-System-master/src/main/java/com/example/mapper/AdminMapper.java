c90599f847454da760a842c68d8efb35b6175b02
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 00:08:47 +0800

    管理员mapper

--- Health-Club-Management-System-master/src/main/java/com/example/mapper/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.pojo.Admin;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminMapper extends BaseMapper<Admin> {
-}

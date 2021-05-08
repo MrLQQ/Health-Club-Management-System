31f9e98883bdc101bc54b1564d2430f68424cde6
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 00:25:42 +0800

    选课mapper

--- Health-Club-Management-System-master/src/main/java/com/example/mapper/PickMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.pojo.Pick;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PickMapper extends BaseMapper<Pick> {
-}

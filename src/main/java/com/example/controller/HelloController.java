301f2dade64e3a8caf5e017a4952c9e0d916fe89
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 00:14:25 +0800

    hello

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -2,12 +2,19 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String indexController(){
+    public String helloController(){
         return "hello";
+    }
+
+    @RequestMapping("/")
+    public ModelAndView indexController(ModelAndView mv){
+        mv.setViewName("index");
+        return mv;
     }
 }

9057c448fa7d32fb5a5330aebfa2fda65cb94d6a
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 00:27:52 +0800

    配置静态资源

---
+++ src/main/java/com/example/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+}

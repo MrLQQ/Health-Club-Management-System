7d8ecc7c3bc4b7e77aaaf2e13e2037c6d4d1db94
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-09 23:37:47 +0800

    添加了index页为欢迎页

--- src/main/java/com/example/controller/JumpController.java
+++ src/main/java/com/example/controller/JumpController.java
@@ -1,13 +1,25 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class JumpController {
 
-    @RequestMapping("/index")
-    public String indexController(){
-        return "index";
+    @RequestMapping(value = {"/","/index"})
+    public ModelAndView indexController(ModelAndView mv,
+                                        @ModelAttribute("msg") String msg,
+                                        HttpServletRequest request){
+        mv.setViewName("index");
+        if (msg.equals(""))
+            msg = "null";
+        System.out.println("msg==>"+msg);
+        request.setAttribute("msg",msg);
+        return mv;
     }
+
 }

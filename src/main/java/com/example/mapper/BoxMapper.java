66dd8a30fad0d84965f2de1b7e7a741f69a2b440
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-14 12:27:36 +0800

    添加自定义SQL，实现多表条件分页联合查询

--- src/main/java/com/example/mapper/BoxMapper.java
+++ src/main/java/com/example/mapper/BoxMapper.java
@@ -1,10 +1,12 @@
 package com.example.mapper;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.pojo.Box;
 import com.example.pojo.BoxUserVO;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +17,7 @@ public interface BoxMapper extends BaseMapper<Box> {
 
     @Select("SELECT Box.*,(select userName from User where Box.userID=User.userID) as userName FROM Box")
     List<BoxUserVO> getBoxInfo(Page<BoxUserVO> page);
+
+    @Select("SELECT Box.*,(select userName from User where Box.userID=User.userID) as userName FROM Box ${ew.customSqlSegment}")
+    List<BoxUserVO> getBoxInfoByconditional(Page<BoxUserVO> page,@Param(Constants.WRAPPER)Wrapper<BoxUserVO> boxUserVOWrapper);
 }

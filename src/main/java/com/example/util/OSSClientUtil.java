c0237556e0e4adee8831ab2afa6cc06e3919d24e
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-20 22:48:56 +0800

    图片上传OSS服务器Util类

---
+++ src/main/java/com/example/util/OSSClientUtil.java
@@ -0,0 +1,206 @@
+package com.example.util;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectResult;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Date;
+import java.util.Random;
+
+@Repository
+public class OSSClientUtil {
+    Log log = LogFactory.getLog(OSSClientUtil.class);
+
+    //阿里云OSS地址，这里看根据你的oss选择
+    protected static String endpoint = "oss-cn-beijing.aliyuncs.com";
+    //阿里云OSS账号
+    protected static String accessKeyId = "LTAI5t6gPJZAgVXs6dZ6NKsa";
+    //阿里云OSS密钥
+    protected static String accessKeySecret = "jl3tqrjqxX2QuR0KzEp0g5gndWHOfD";
+    //阿里云OSS上的存储块bucket名字
+    protected static String bucketName = "health-club-management-system";
+
+    //阿里云图片文件存储目录
+    private String homeimagedir = "image/";
+
+    private OSSClient ossClient;
+
+    public OSSClientUtil() {
+        ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+    }
+
+    /**
+     * 初始化
+     */
+    public void init() {
+        ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+    }
+
+    /**
+     * 销毁
+     */
+    public void destory() {
+        ossClient.shutdown();
+    }
+
+    /**
+     * 图片 上传阿里云oss
+     *
+     * @param file
+     * @return
+     */
+    public String uploadHomeImageOSS(MultipartFile file) throws Exception {
+        if (file.getSize() > 1024 * 1024 * 10) {
+            throw new Exception("上传图片大小不能超过10M！");
+        }
+        String originalFilename = file.getOriginalFilename();
+        String substring = originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
+        Random random = new Random();
+        String name = random.nextInt(10000) + System.currentTimeMillis() + substring;
+        System.out.println("fileName=>"+name);
+        try {
+            InputStream inputStream = file.getInputStream();
+            this.uploadHomeImageFileOSS(inputStream, name);
+            return name;
+        } catch (Exception e) {
+            throw new Exception("图片上传失败");
+        }
+    }
+
+    /**
+     * 获得图片路径
+     *
+     * @param fileUrl
+     * @return
+     */
+    public String getHomeImageUrl(String fileUrl) {
+        System.out.println("fileUrl"+fileUrl);
+        if (!StringUtils.isEmpty(fileUrl)) {
+            String[] split = fileUrl.split("/");
+            return this.getUrl(this.homeimagedir + split[split.length - 1]);
+        }
+        return null;
+    }
+
+    /**
+     * 图片上传到OSS服务器  如果同名文件会覆盖服务器上的
+     *
+     * @param instream 文件流
+     * @param fileName 文件名称 包括后缀名
+     * @return 出错返回"" ,唯一MD5数字签名
+     */
+    public String uploadHomeImageFileOSS(InputStream instream, String fileName) {
+        String ret = "";
+        try {
+            //创建上传Object的Metadata
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(instream.available());
+            objectMetadata.setCacheControl("no-cache");
+            objectMetadata.setHeader("Pragma", "no-cache");
+            objectMetadata.setContentType(getcontentType(fileName.substring(fileName.lastIndexOf("."))));
+            objectMetadata.setContentDisposition("inline;filename=" + fileName);
+            //上传文件
+            PutObjectResult putResult = ossClient.putObject(bucketName, homeimagedir + fileName, instream, objectMetadata);
+            ret = putResult.getETag();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            try {
+                if (instream != null) {
+                    instream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * 判断OSS服务文件上传时文件的类型contentType
+     *
+     * @param FilenameExtension 文件后缀
+     * @return String
+     */
+    public static String getcontentType(String FilenameExtension) {
+        if (FilenameExtension.equalsIgnoreCase(".bmp")) {
+            return "image/bmp";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".gif")) {
+            return "image/gif";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".jpeg") ||
+                FilenameExtension.equalsIgnoreCase(".jpg") ||
+                FilenameExtension.equalsIgnoreCase(".png")) {
+            return "image/jpeg";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".html")) {
+            return "text/html";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".txt")) {
+            return "text/plain";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".vsd")) {
+            return "application/vnd.visio";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".pptx") ||
+                FilenameExtension.equalsIgnoreCase(".ppt")) {
+            return "application/vnd.ms-powerpoint";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".docx") ||
+                FilenameExtension.equalsIgnoreCase(".doc")) {
+            return "application/msword";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".xml")) {
+            return "text/xml";
+        }
+        return "image/jpg";
+    }
+
+    /**
+     * 获得url链接
+     *
+     * @param key
+     * @return
+     */
+    public String getUrl(String key) {
+        System.out.println("key==>"+key);
+        // 设置URL过期时间为10年  3600l* 1000*24*365*10
+        Date expiration = new Date(System.currentTimeMillis() + 3600L * 1000 * 24 * 365 * 10);
+        // 生成URL
+        URL url = ossClient.generatePresignedUrl(bucketName, key, expiration);
+        if (url != null) {
+            String urlToString=url.toString();
+            System.out.println("urlTOString==>"+urlToString);
+            // 截取url
+            String urls[]=urlToString.split("\\?");
+            return urls[0];
+        }
+        return null;
+    }
+
+    /**
+     * 判断图片
+     *
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    public String updateHomeImage(MultipartFile file) throws Exception {
+
+        OSSClientUtil ossClient = new OSSClientUtil();
+        if (file == null || file.getSize() <= 0) {
+            throw new Exception("图片不能为空");
+        }
+        String name = ossClient.uploadHomeImageOSS(file);
+        String imgUrl = ossClient.getHomeImageUrl(name);
+        return imgUrl;
+    }
+}

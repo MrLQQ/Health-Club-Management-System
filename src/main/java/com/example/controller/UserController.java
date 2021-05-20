50139fd81bf244940ffe2487c8486ebd28dbf80b
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-20 22:51:50 +0800

    新增用户的信息修改、删除、上传头像业务

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.mapper.UserMapper;
+import com.example.pojo.Emp;
 import com.example.pojo.User;
+import com.example.util.OSSClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,16 +13,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    OSSClientUtil ossClientUtil;
 
     @Autowired
     private UserMapper userMapper;
@@ -30,16 +36,18 @@ public class UserController {
                               @RequestParam(name = "userName") String userName,
                               @RequestParam(name = "password") String password, RedirectAttributes attr){
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        userQueryWrapper.select("password")
-                .eq("userName",userName);
+        userQueryWrapper.eq("userName",userName);
         List<Map<String, Object>> maps =userMapper.selectMaps(userQueryWrapper);
         String SelectPassword="";
+        String image="";
         for (Map<String, Object> map : maps) {
             SelectPassword= (String) map.get("password");
+            image = (String) map.get("image");
         }
         if (password.equals(SelectPassword)){
             mv.setViewName("userIndex");
             mv.addObject("userName",userName);
+            mv.addObject("image",image);
             return mv;
         }
         else{
@@ -49,6 +57,17 @@ public class UserController {
         }
     }
 
+    /**
+     * 用户注册
+     * @param mv
+     * @param userName
+     * @param realName
+     * @param password
+     * @param sex
+     * @param phone
+     * @param attr
+     * @return ModelAndView
+     */
     @PostMapping("/registered")
     public ModelAndView registered(ModelAndView mv,
                                    @RequestParam(name = "userName") String userName,
@@ -65,6 +84,8 @@ public class UserController {
         user.setSex(sex);
         user.setPhone(phone);
         user.setUserIdent("非会员");
+        // 设置默认头像
+        user.setImage("https://health-club-management-system.oss-cn-beijing.aliyuncs.com/image/0000000000000.png");
 
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
         userQueryWrapper.select("userName")
@@ -90,11 +111,12 @@ public class UserController {
             map.addAttribute("user",user);
         }
 
-        return "user";
+        return "userCenter";
     }
 
-    @PostMapping("/update")
-    public String userUpdate(@RequestParam(name="userID",required = true) String userID,
+    @PostMapping("/userUpdate")
+    public String userUpdate(@RequestParam(value = "image") MultipartFile file,
+                             @RequestParam(name="userID",required = true) String userID,
                              @RequestParam(name="userName",required = true) String userName,
                              @RequestParam(name="password",required = true) String password,
                              @RequestParam(name="sex",required = true) String sex,
@@ -102,11 +124,25 @@ public class UserController {
                              ModelMap map
                              ){
         // 在数据库中查找出目标的user对象
+        String msg="";
         User user = userMapper.selectById(userID);
         user.setUserName(userName);
         user.setPassword(password);
         user.setSex(sex);
         user.setPhone(phone);
+        if (file != null || file.getSize() > 0){
+            System.out.println("file's sizeof==》"+file.getSize());
+            if (file.getSize() > 1024 * 1024 * 10){
+                msg+="图片不能超过10MB!";
+            }
+            else {
+                try {
+                    user.setImage(ossClientUtil.updateHomeImage(file));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         // 查找目标要修改的userName在数据库中是否存在
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
@@ -124,9 +160,10 @@ public class UserController {
                 else {
                     System.out.println("id不相同:"+entity.getUserID()+"=="+userID);
                     // 如果两个ID不相同，说明如果修改userName则会在数据库中出现重复项，所以禁止修改。
-                    map.addAttribute("msg","用户名已存在，请重新修改");
+                    msg+=" 用户名已存在，请重新修改!";
+                    map.addAttribute("msg",msg);
                     map.addAttribute("user",userMapper.selectById(userID));
-                    return "user";
+                    return "userCenter";
                 }
             }
         }
@@ -134,7 +171,7 @@ public class UserController {
         userMapper.updateById(user);
         map.addAttribute("msg","修改成功");
         map.addAttribute("user",user);
-        return "user";
+        return "userCenter";
     }
 
     // 用户管理页面
@@ -170,16 +207,21 @@ public class UserController {
                                                     RedirectAttributes attr){
         Page<User> page =new Page<>(pageNum,10);
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        if (!userID.equals(""))
+        if (!userID.equals("")) {
             userQueryWrapper.eq("userID",userID);
-        if (!userName.equals(""))
+        }
+        if (!userName.equals("")) {
             userQueryWrapper.eq("userName",userName);
-        if (!realName.equals(""))
+        }
+        if (!realName.equals("")) {
             userQueryWrapper.eq("realName",realName);
-        if (!userIdent.equals(""))
+        }
+        if (!userIdent.equals("")) {
             userQueryWrapper.eq("userIdent",userIdent);
-        if (!sex.equals(""))
+        }
+        if (!sex.equals("")) {
             userQueryWrapper.eq("sex",sex);
+        }
 
         userMapper.selectPage(page,userQueryWrapper);
 
@@ -204,6 +246,58 @@ public class UserController {
             modelAndView.setViewName("redirect:/user/1");
             attr.addFlashAttribute("msg","未查找到有效数据，请修改条件后重新查询");
         }
+        return modelAndView;
+    }
+
+    @RequestMapping("/edit/{userID}")
+    public ModelAndView userEdit(@PathVariable("userID") Long userID,ModelAndView modelAndView){
+        modelAndView.setViewName("userUpdate");
+        User user = userMapper.selectById(userID);
+        modelAndView.addObject("user",user);
+        return modelAndView;
+    }
+
+    @RequestMapping("/update")
+    public ModelAndView userUpdate(ModelAndView modelAndView,
+                                  @RequestParam(name = "userID",required = true) Long userID,
+                                  @RequestParam(name = "sex", required = true) String sex,
+                                  @RequestParam(name = "userIdent", required = true) String userIdent,
+                                  @RequestParam(name = "phone", required = true) String phone){
+        User user = new User();
+        user.setUserID(userID);
+        user.setSex(sex);
+        user.setUserIdent(userIdent);
+        user.setPhone(phone);
+        int update = userMapper.updateById(user);
+        if (update>=1){
+            modelAndView.setViewName("redirect:/user/1");
+            return modelAndView;
+        }
+        modelAndView.setViewName("userUpdate");
+        modelAndView.addObject("user",userMapper.selectById(userID));
+        modelAndView.addObject("msg","修改失败！");
+        return modelAndView;
+    }
+
+    @RequestMapping("/delete/{userID}")
+    public ModelAndView userDelete(ModelAndView modelAndView,
+                                  @PathVariable("userID") Long userID,
+                                   RedirectAttributes attr){
+
+        // 由于选课表或者租赁表的其中的外键都为userID
+        // 所以在删除时候可能出现违反唯一约束的异常
+        try{
+            userMapper.deleteById(userID);
+        }catch (Exception e){
+            Throwable cause = e.getCause();
+            // 判断是否为’违反唯一约束‘异常
+            if (cause instanceof SQLIntegrityConstraintViolationException){
+                attr.addFlashAttribute("msg","删除失败，可能该用户的服务还未结束，可能已选课或租赁储物柜！");
+                modelAndView.setViewName("redirect:/user/1");
+                return modelAndView;
+            }
+        }
+        modelAndView.setViewName("redirect:/user/1");
         return modelAndView;
     }
 }

35f894f34b06e39d3899729b29aa1a2c94cf71aa
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-20 22:42:53 +0800

    增加了课程的修改、删除业务

--- src/main/java/com/example/controller/CourseController.java
+++ src/main/java/com/example/controller/CourseController.java
@@ -4,8 +4,10 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.mapper.CourseMapper;
 import com.example.mapper.EmpMapper;
+import com.example.pojo.Course;
 import com.example.pojo.CourseEmpVO;
 import com.example.pojo.Emp;
+import com.example.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
+import java.util.Queue;
 
 @Controller
 @RequestMapping("/course")
@@ -62,10 +66,12 @@ public class CourseController {
         QueryWrapper<CourseEmpVO> courseEmpVOQueryWrapper = new QueryWrapper<>();
         QueryWrapper<Emp> empQueryWrapper = new QueryWrapper<>();
 
-        if (!courseID.equals(""))
+        if (!courseID.equals("")) {
             courseEmpVOQueryWrapper.eq("courseID",courseID);
-        if (!courseName.equals(""))
+        }
+        if (!courseName.equals("")) {
             courseEmpVOQueryWrapper.eq("courseName",courseName);
+        }
         if (!empName.equals("")){
             empQueryWrapper.select("empID").eq("empName",empName);
             Emp emp = empMapper.selectOne(empQueryWrapper);
@@ -95,6 +101,75 @@ public class CourseController {
             attr.addFlashAttribute("msg","未查找到有效数据，请修改条件后重新查询");
         }
 
+        return modelAndView;
+    }
+
+    @RequestMapping("/edit/{courseID}")
+    public ModelAndView courseEdit(@PathVariable("courseID") Long courseID,ModelAndView modelAndView){
+        modelAndView.setViewName("courseUpdate");
+        Course course = courseMapper.selectById(courseID);
+        QueryWrapper<Emp> empQueryWrapper =new QueryWrapper<>();
+        empQueryWrapper.select("empName")
+                .eq("empID",course.getEmpID());
+        Emp emp = empMapper.selectOne(empQueryWrapper);
+
+        modelAndView.addObject("course",course);
+        modelAndView.addObject("empName",emp.getEmpName());
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/update")
+    public ModelAndView userUpdate(ModelAndView modelAndView,
+                                   @RequestParam(name = "courseID",required = true) Long courseID,
+                                   @RequestParam(name = "courseName", required = true) String courseName,
+                                   @RequestParam(name = "empName", required = true) String empName,
+                                   @RequestParam(name = "time", required = true) String time,
+                                   @RequestParam(name = "residue", required = true) Integer residue){
+
+        Course course = new Course();
+        course.setCourseID(courseID);
+        course.setCourseName(courseName);
+        course.setTime(time);
+        course.setResidue(residue);
+
+        QueryWrapper<Emp> empQueryWrapper = new QueryWrapper<>();
+        empQueryWrapper.eq("empName",empName);
+        Emp emp = empMapper.selectOne(empQueryWrapper);
+        if (emp!=null){
+            course.setEmpID(emp.getEmpID());
+            int update = courseMapper.updateById(course);
+            if (update>=1){
+                modelAndView.setViewName("redirect:/course/1");
+                return modelAndView;
+            }
+        }
+        modelAndView.setViewName("courseUpdate");
+        modelAndView.addObject("empName",empName);
+        modelAndView.addObject("course",course);
+        modelAndView.addObject("msg","修改失败,当前教练不存在！");
+        return modelAndView;
+    }
+
+    @RequestMapping("/delete/{courseID}")
+    public ModelAndView userDelete(ModelAndView modelAndView,
+                                   @PathVariable("courseID") Long courseID,
+                                   RedirectAttributes attr){
+
+        // 由于选课表或者租赁表的其中的外键都为userID
+        // 所以在删除时候可能出现违反唯一约束的异常
+        try{
+            courseMapper.deleteById(courseID);
+        }catch (Exception e){
+            Throwable cause = e.getCause();
+            // 判断是否为’违反唯一约束‘异常
+            if (cause instanceof SQLIntegrityConstraintViolationException){
+                attr.addFlashAttribute("msg","删除失败，可能该用户的服务还未结束，可能已选课或租赁储物柜！");
+                modelAndView.setViewName("redirect:/course/1");
+                return modelAndView;
+            }
+        }
+        modelAndView.setViewName("redirect:/course/1");
         return modelAndView;
     }
 }

1be21aad8667b1e47f6517a6f53d5f25e8dff9ab
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-29 20:46:13 +0800

    增加了跳转主页时附带课程信息

--- src/main/java/com/example/controller/JumpController.java
+++ src/main/java/com/example/controller/JumpController.java
@@ -1,22 +1,46 @@
 package com.example.controller;
 
+import com.example.mapper.CourseMapper;
+import com.example.pojo.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class JumpController {
+
+    @Autowired
+    CourseMapper courseMapper;
 
     @RequestMapping(value = {"/","/index"})
     public ModelAndView indexController(ModelAndView mv,
                                         @ModelAttribute("msg") String msg,
                                         HttpServletRequest request){
+
+        List<Course> courses = courseMapper.selectList(null);
+        mv.addObject("courses",courses);
         mv.setViewName("index");
-        if (msg.equals(""))
+        if (msg.equals("")) {
             msg = "null";
+        }
+        System.out.println("msg==>"+msg);
+        request.setAttribute("msg",msg);
+        return mv;
+    }
+
+    @RequestMapping(value = {"/test"})
+    public ModelAndView testController(ModelAndView mv,
+                                        @ModelAttribute("msg") String msg,
+                                        HttpServletRequest request){
+        mv.setViewName("test");
+        if (msg.equals("")) {
+            msg = "null";
+        }
         System.out.println("msg==>"+msg);
         request.setAttribute("msg",msg);
         return mv;

acd7aa4cdda48e0a37e03521d23b89dc66d4098b
2802351094@qq.com <MrLQQ@users.noreply.github.com>
2021-05-29 20:49:46 +0800

    增加Pick表、Course表、User表联合查询方法

--- src/main/java/com/example/mapper/PickMapper.java
+++ src/main/java/com/example/mapper/PickMapper.java
@@ -1,9 +1,18 @@
 package com.example.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.example.pojo.Pick;
+import com.example.pojo.PickCourseUserVO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PickMapper extends BaseMapper<Pick> {
+
+    @Select("SELECT Pick.*,(select userName from User where Pick.userID=User.userID) as userName,(select courseName from Course where Pick.courseID=Course.courseID) as courseName FROM Pick ${ew.customSqlSegment}")
+    List<PickCourseUserVO> getPickInfo(@Param(Constants.WRAPPER) Wrapper<PickCourseUserVO> voWrapper);
 }
